371541e9bee5631b683a12432b3f974706ed2193
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2021-08-18 16:45:58 -0300

    Comentando funcionalidades dos widgets usados no showDialog

--- lib/utils/alert.dart
+++ lib/utils/alert.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 alert(BuildContext context, String message) {
   showDialog(
       context: context,
-      barrierDismissible: false,
+      barrierDismissible: false, //não fechar o dialog clicando por fora
       builder: (context) {
+        //Widget para evitar que o dialog seja fechado pelo botão de voltar do android
         return WillPopScope(
+          //função que será chamada quando o botão do android de voltar for clicado
           onWillPop: () async => false,
           child: AlertDialog(
             title: Text("Aplicativo de Carros"),
