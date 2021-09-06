8a4560ff391121f3686ae0008e0a9f8262014322
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2021-09-06 12:21:00 -0300

    Criando o controller da TabBar da home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,7 +9,23 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin<HomePage> {
+
+  late TabController _tabBarController;
+
+  //construtor de um statefull Widget, é chamado apenas uma vez quando o componente é renderizado 
+  //ou seja, não é executado novamente quando o setState é chamado 
+  @override
+  void initState() {
+    super.initState();
+    _tabBarController = TabController(
+        length: 3,
+        vsync: this /*passando o mixin do widget via palavra "this"*/);
+
+        _tabBarController.index = 1;
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -18,17 +34,32 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
         appBar: AppBar(
           title: Text("Carros Home Page"),
           centerTitle: true,
-          bottom: TabBar(tabs: [
-            Tab(text: "Clássicos",),
-            Tab(text: "Esportivos",),
-            Tab(text: "Luxo",),
-          ],),
+          bottom: TabBar(
+            controller: _tabBarController,
+            tabs: [
+              Tab(
+                text: "Clássicos",
+              ),
+              Tab(
+                text: "Esportivos",
+              ),
+              Tab(
+                text: "Luxo",
+              ),
+            ],
+          ),
         ),
         drawer: DrawerList(),
-        body: TabBarView(children: [
-          CarrosListView(tipo: describeEnum(TipoCarro.classicos),),
-          CarrosListView(tipo: describeEnum(TipoCarro.esportivos),),
-          CarrosListView(tipo: describeEnum(TipoCarro.luxo),),
+        body: TabBarView(controller: _tabBarController, children: [
+          CarrosListView(
+            tipo: describeEnum(TipoCarro.classicos),
+          ),
+          CarrosListView(
+            tipo: describeEnum(TipoCarro.esportivos),
+          ),
+          CarrosListView(
+            tipo: describeEnum(TipoCarro.luxo),
+          ),
         ]),
       ),
     );
